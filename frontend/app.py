0889f6492d877ea56603d7d5163fc624be8b696a
Maazin-Ahmed <144589820+Maazin-Ahmed@users.noreply.github.com>
2025-06-20 17:24:50 +0530

    Custom port added

--- frontend/app.py
+++ frontend/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template , request
 
+BACKEND_URL = 'http://0.0.0.0:5000'
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,4 +9,4 @@ def home():
     return render_template('todo.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=6000)

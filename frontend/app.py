0ecd0c45f5f9020cbc2332589cd54cee62455dbf
Maazin Ahmed <ahmed.maazin7@gmail.com>
2025-06-20 17:47:26 +0530

    Item ID Added

--- frontend/app.py
+++ frontend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template , request
 
-BACKEND_URL = 'http://0.0.0.0:5000'
+#BACKEND_URL = 'http://0.0.0.0:5000'
 
 app = Flask(__name__)
 
@@ -9,4 +9,4 @@ def home():
     return render_template('todo.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=6000)
+    app.run(debug=True, )#host='0.0.0.0', port=6000
